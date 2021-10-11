29ba61316737661d2299a63012a6ce4ce0aaeae0
kbventures <kmb_x@hotmail.com>
2021-10-11 20:10:02 +0200

    Fixed image url error

---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div>
+      <header id="header" class="text-center">
+        <h1 id="title">KBVentures Feedback Form</h1>
+        <p id="description">
+          Thank you for taking the time to help provide insight into the quality
+          of our github code
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
